8d3b2514b8987b78d0099201afb78f3007a39df9
Keith Aleq Jackson <i@keithajackson.com>
2016-02-06 23:37:15 -0800

    Refactor update scripts to improve readability

--- terminal/bash/update.sh
+++ terminal/bash/update.sh
@@ -1,32 +1,51 @@
-updatedotfiles() {
+__update_dot_files() {
   prompt_verbose 'Checking for updates to dotfiles...'
-	# if our dotfiles are up to date, check for updates
+
+	# if we have unsaved changes in our dotfiles, we can't update.
 	if [ -n "$(cd $DOTFILES_DIR && git diff)" ]; then
 		prompt_err 'Dotfiles have working directory changes; skipping check for updates.'
-	else
-		prompt_verbose "$(git fetch origin)"
-		if [ -n "$(git status | grep behind)" ]; then
-			prompt_verbose 'Updating dotfiles...'
-			local update_output="$(git merge --ff-only)"
-			if [ $? != 0 ]; then
-				prompt_err 'Could not update dotfiles as there are merge conflicts.'
-      else
-        if [ -n "$update_output" ]; then
-          prompt_verbose $update_output
-          prompt_notify 'Dotfiles updated'
-        else
-          prompt_notify 'Dotfiles up-to-date.'
-        fi
-			fi
-		fi
-	fi
+    return
+  fi
+
+  # Only display get fetch if we're verbose logging
+  prompt_verbose "$(git fetch origin)"
+
+  # If we aren't behind master, there's nothing to update.
+  # Notify and we're done
+  if [ -z "$(git status | grep behind)" ]; then
+      prompt_verbose 'Dotfiles up-to-date.'
+      return
+  fi
+
+  # Attempt to update, but only if we can do so without hitting
+  # merge conflicts.
+  prompt_verbose 'Updating dotfiles...'
+  prompt_verbose "$(git merge --ff-only)"
+
+  # If the update fails with a nonzero error code, tell the user
+  if [ $? != 0 ]; then
+    prompt_err 'Could not update dotfiles as there are merge conflicts.'
+    return
+  fi
+
+  prompt_notify 'Dotfiles updated.'
 }
 
-updatebrew() {
+__update_brew() {
   prompt_verbose 'Checking for new brew packages...'
-	prompt_verbose "$(brew upgrade --all)"
-  prompt_notify 'Brew packages up-to-date.'
+
+
+	local brew_output="$(brew upgrade --all)"
+  prompt_verbose $brew_output
+
+  # If brew output nothing, then it was already up-to-date.
+  if [ -z "$brew_output" ]; then
+    prompt_verbose 'Brew packages up-to-date.'
+    return
+  fi
+
+  prompt_notify 'Brew packages updated.'
 }
 
-updatedotfiles
-updatebrew
+__update_dot_files
+__update_brew
